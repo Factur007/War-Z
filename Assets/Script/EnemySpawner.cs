e62fdea383e1ff6b2ad550539291e0667c77820e
Factur007 <mark.doronin.09@mail.ru>
2024-05-24 19:05:20 +0300

    1

--- Assets/Script/EnemySpawner.cs
+++ Assets/Script/EnemySpawner.cs
@@ -8,6 +8,7 @@ public class NewBehaviourScript : MonoBehaviour
     public int numberOfEnemies;
     public float zRandValue = 5;
     public float xRandValue = 5;
+    private int enemyCount;
 
     void Start()
     {
@@ -16,12 +17,23 @@ public class NewBehaviourScript : MonoBehaviour
         
     }
 
+   
     void SpawnEnemies()
     {
         for (int i = 0; i < numberOfEnemies; i++)
         {
             Vector3 randomPosition = new Vector3(Random.Range( -xRandValue, xRandValue), 0, Random.Range( -zRandValue,zRandValue)) + transform.position;    
             GameObject newEnemy = Instantiate(enemyPrefab, randomPosition, Quaternion.identity);
+        }
+    }
+
+    private void Update()
+    {
+        enemyCount = FindObjectsOfType<Wrag>().Length;
+        if (enemyCount == 0 )
+        {
+            numberOfEnemies++;
+            SpawnEnemies();
         }
     }
 }
